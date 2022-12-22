c82cfa34ba97d87c524351910f669c1cbe5b840f
Zarok23 <sarok231294@gmail.com>
2022-12-22 16:26:05 +0300

    create project

---
+++ src/main/java/com/example/imageUI/ImageUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.imageUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageUiApplication.class, args);
+	}
+
+}
